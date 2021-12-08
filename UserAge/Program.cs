a58129685102a6c1ce3a2b9408d7e744bb4ad947
Mihkel Lember <milember@tktk.ee>
2021-12-08 02:52:14 +0200

    Control Flow

---
+++ UserAge/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserAge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutaja vanust;
+            //programm arvutab kasutaja sünniaastat;
+            //programm kuvab sünniaastat konsoolis
+            //"oled sündinud aastal {yearOfBirth}"
+
+            Console.WriteLine("Palun sisesta oma vanus:");
+
+            int userAge = Convert.ToInt32(Console.ReadLine());
+
+            int yearOfBirth = 2021 - userAge;
+
+            Console.WriteLine($"Oled sündinud aastal {yearOfBirth}.");
+
+
+        }
+    }
+}
